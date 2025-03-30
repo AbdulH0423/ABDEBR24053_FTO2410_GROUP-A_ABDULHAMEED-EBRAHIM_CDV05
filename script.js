b1c9b7d9d11075fccab072549c7408fe3426ded7
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-03-30 14:44:06 +0200

    removed placeholder code

--- script.js
+++
@@ -1,6 +0,0 @@
-// Log when a project link is clicked
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        console.log(`You clicked on ${button.previousElementSibling.innerText}`);
-    });
-});
